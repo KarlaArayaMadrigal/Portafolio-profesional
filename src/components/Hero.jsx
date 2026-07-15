337ebd9737776c36f471ec5dbe3f2833f6cfc59d
Plataformas Abiertas <kd.araya.m@gmail.com>
2026-07-14 22:39:14 -0600

    Creación de Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,50 @@
+import styled from "styled-components";
+
+
+const Section = styled.section`
+
+height:100vh;
+
+display:flex;
+justify-content:center;
+align-items:center;
+
+text-align:center;
+
+padding:0 20px;
+background-color: #010407;
+
+`;
+
+
+const Title = styled.h1`
+
+font-size:55px;
+
+
+span{
+
+color:#38BDF8;
+
+}
+
+
+`;
 function Hero (){
    return <>
+   <Section>
+
+    <div>
+
+    <Title>
+        Hola, me llamo 
+        
+        <span>
+            Karla Araya
+        </span>
+    </Title>
+    </div>
+   </Section>
     </>
 } 
 export default Hero;
